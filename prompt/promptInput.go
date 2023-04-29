9df39336beceaa0c5a9bed224697ecb4c4740c0a
Rio Arswendo <r.arswendo.r@gmail.com>
2023-04-29 16:05:13 +0700

    feat(prompt): added helper utils for prompting user

---
+++ prompt/promptInput.go
@@ -0,0 +1,36 @@
+package prompt
+
+import (
+	"errors"
+	"fmt"
+	"os"
+
+	"github.com/manifoldco/promptui"
+)
+
+type PromptContent struct {
+	ErrorMessage string
+	Label        string
+}
+
+func PromptGetInput(pc PromptContent) string {
+	validate := func(input string) error {
+		if input == "" {
+			return errors.New(pc.ErrorMessage)
+		}
+		return nil
+	}
+
+	prompt := promptui.Prompt{
+		Label:    pc.Label,
+		Validate: validate,
+	}
+
+	result, err := prompt.Run()
+	if err != nil {
+		fmt.Printf("Prompt failed %v\n", err)
+		os.Exit(1)
+	}
+
+	return result
+}
